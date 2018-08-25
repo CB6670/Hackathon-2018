9e481e6663a9a7b6d56ef98b42ba26b4a05ff255
eyesofmara <lynseybond@gmail.com>
2018-08-25 13:55:41 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,478 @@
+contactData.forEach(function (element) {
+    // create and append table row
+    let row = document.createElement("tr");
+    mainTable.appendChild(row);
+    // create cells for row
+    let picture = document.createElement("td");
+    let name = document.createElement("td");
+    let email = document.createElement("td");
+    let phone = document.createElement("td");
+    let address = document.createElement("td");
+    
+    // append cells to row
+    row.appendChild(firstCell);
+    row.appendChild(lastCell);
+    row.appendChild(emailCell);
+    // add text to cells
+    pictureCell.innterHTML = element["picture"]
+    nameCell.innerHTML = element["name"]; // what to do about the name object with first and last
+    compCell.innerHTML = element["company"];
+    emailCell.innerHTML = element["email"];
+    phoneCell.innerHTML = element["phone"];
+    addressCell.innerHTML = element["address"];
+});
+
+
+
+let contactData = [
+    {
+      "_id": "5b748199153ccafc68fc6459",
+      "isActive": true,
+      "picture": "http://placehold.it/64x64",
+      "age": 38,
+      "name": {
+       "first": "Torres",
+        "last": "Cotton"
+      },
+      "company": "PIGZART",
+      "email": "torres.cotton@pigzart.co.uk",
+      "phone": "+1 (985) 510-3442",
+      "address": "119 Whitney Avenue, Katonah, Kansas, 8013",
+      "registered": "Thursday, September 17, 2015 12:15 AM"
+    },
+    {
+      "_id": "5b74819912c50069bfc57215",
+      "isActive": false,
+      "picture": "http://placehold.it/64x64",
+      "age": 20,
+      "name": {
+        "first": "Moran",
+        "last": "Mccarty"
+      },
+      "company": "GOGOL",
+      "email": "moran.mccarty@gogol.org",
+      "phone": "+1 (860) 494-2342",
+      "address": "165 Lawn Court, Toftrees, Oregon, 3144",
+      "registered": "Saturday, July 5, 2014 12:23 PM"
+    },
+    {
+      "_id": "5b748199379666d9fcbe4c8b",
+      "isActive": false,
+      "picture": "http://placehold.it/64x64",
+      "age": 44,
+      "name": {
+        "first": "Leola",
+        "last": "Hubbard"
+      },
+      "company": "VORATAK",
+      "email": "leola.hubbard@voratak.tv",
+      "phone": "+1 (813) 567-2856",
+      "address": "843 Calder Place, Blue, Palau, 7030",
+      "registered": "Thursday, April 14, 2016 2:03 PM"
+    },
+    {
+      "_id": "5b748199ca5cf0eca1d6408a",
+      "isActive": false,
+      "picture": "http://placehold.it/64x64",
+      "age": 37,
+      "name": {
+        "first": "Monique",
+        "last": "Barrett"
+      },
+      "company": "INSURETY",
+      "email": "monique.barrett@insurety.ca",
+      "phone": "+1 (910) 411-2802",
+      "address": "982 Buffalo Avenue, Grandview, Connecticut, 325",
+      "registered": "Monday, April 13, 2015 5:56 PM"
+    },
+    {
+      "_id": "5b748199cf64f25560f2dba2",
+      "isActive": true,
+      "picture": "http://placehold.it/64x64",
+      "age": 38,
+      "name": {
+        "first": "Cameron",
+        "last": "Mcmillan"
+      },
+      "company": "TELEQUIET",
+      "email": "cameron.mcmillan@telequiet.us",
+      "phone": "+1 (883) 443-2199",
+      "address": "536 Kermit Place, Gulf, Vermont, 7771",
+      "registered": "Thursday, November 23, 2017 8:03 PM"
+    },
+    {
+      "_id": "5b748199c108710dd11e88d3",
+      "isActive": true,
+      "picture": "http://placehold.it/64x64",
+      "age": 41,
+      "name": {
+        "first": "Linda",
+        "last": "Sexton"
+      },
+      "company": "ATGEN",
+      "email": "linda.sexton@atgen.net",
+      "phone": "+1 (911) 548-3707",
+      "address": "563 Leonora Court, Unionville, Arizona, 2077",
+      "registered": "Wednesday, June 24, 2015 9:41 PM"
+    },
+    {
+      "_id": "5b7481990356983ad55ce6d6",
+      "isActive": false,
+      "picture": "http://placehold.it/64x64",
+      "age": 34,
+      "name": {
+        "first": "Freida",
+        "last": "Sellers"
+      },
+      "company": "SONIQUE",
+      "email": "freida.sellers@sonique.io",
+      "phone": "+1 (878) 501-2257",
+      "address": "894 Wortman Avenue, Faxon, New York, 8394",
+      "registered": "Sunday, January 8, 2017 10:00 PM"
+    },
+    {
+      "_id": "5b7481994553522cdfa48d2e",
+      "isActive": false,
+      "picture": "http://placehold.it/64x64",
+      "age": 44,
+      "name": {
+        "first": "England",
+        "last": "Harrison"
+      },
+      "company": "ZIGGLES",
+      "email": "england.harrison@ziggles.info",
+      "phone": "+1 (804) 429-3542",
+      "address": "110 Gem Street, Golconda, Montana, 5311",
+      "registered": "Monday, May 12, 2014 12:26 PM"
+    },
+    {
+      "_id": "5b748199628355e635fa2967",
+      "isActive": true,
+      "picture": "http://placehold.it/64x64",
+      "age": 30,
+      "name": {
+        "first": "Hatfield",
+        "last": "Allen"
+      },
+      "company": "QUAREX",
+      "email": "hatfield.allen@quarex.biz",
+      "phone": "+1 (831) 538-3450",
+      "address": "990 Holly Street, Allamuchy, Washington, 4403",
+      "registered": "Friday, June 30, 2017 6:11 PM"
+    },
+    {
+      "_id": "5b74819952dee673faf2f7ef",
+      "isActive": false,
+      "picture": "http://placehold.it/64x64",
+      "age": 29,
+      "name": {
+        "first": "Joanne",
+        "last": "Garner"
+      },
+      "company": "EXTREMO",
+      "email": "joanne.garner@extremo.name",
+      "phone": "+1 (868) 434-3628",
+      "address": "852 Rose Street, Gambrills, Missouri, 9050",
+      "registered": "Friday, January 6, 2017 8:02 AM"
+    },
+    {
+      "_id": "5b748199528f43148abd3a4c",
+      "isActive": false,
+      "picture": "http://placehold.it/64x64",
+      "age": 45,
+      "name": {
+        "first": "Elsa",
+        "last": "Hill"
+      },
+      "company": "BLUPLANET",
+      "email": "elsa.hill@bluplanet.me",
+      "phone": "+1 (854) 424-2342",
+      "address": "531 Baltic Street, Tioga, Marshall Islands, 9085",
+      "registered": "Thursday, April 14, 2016 9:24 AM"
+    },
+    {
+      "_id": "5b7481997d59700758741dec",
+      "isActive": false,
+      "picture": "http://placehold.it/64x64",
+      "age": 48,
+      "name": {
+        "first": "Baker",
+        "last": "Rush"
+      },
+      "company": "PLASMOS",
+      "email": "baker.rush@plasmos.biz",
+      "phone": "+1 (846) 533-2376",
+      "address": "830 Rock Street, Weogufka, Guam, 3086",
+      "registered": "Friday, April 27, 2018 11:53 PM"
+    },
+    {
+      "_id": "5b748199a28cc2972a702af2",
+      "isActive": true,
+      "picture": "http://placehold.it/64x64",
+      "age": 20,
+      "name": {
+        "first": "Marylou",
+        "last": "Foster"
+      },
+      "company": "KYAGURU",
+      "email": "marylou.foster@kyaguru.co.uk",
+      "phone": "+1 (876) 465-3670",
+      "address": "183 Hart Street, Dupuyer, New Hampshire, 8604",
+      "registered": "Monday, December 25, 2017 6:27 PM"
+    },
+    {
+      "_id": "5b7481997e6fbd3704e73550",
+      "isActive": false,
+      "picture": "http://placehold.it/64x64",
+      "age": 43,
+      "name": {
+        "first": "Salinas",
+        "last": "Hodges"
+      },
+      "company": "RETROTEX",
+      "email": "salinas.hodges@retrotex.org",
+      "phone": "+1 (871) 515-3483",
+      "address": "109 Cleveland Street, Weedville, Maryland, 654",
+      "registered": "Wednesday, June 10, 2015 10:40 AM"
+    },
+    {
+      "_id": "5b748199529002fdcc4c7e8b",
+      "isActive": true,
+      "picture": "http://placehold.it/64x64",
+      "age": 21,
+      "name": {
+        "first": "Claire",
+        "last": "Fischer"
+      },
+      "company": "SNIPS",
+      "email": "claire.fischer@snips.tv",
+      "phone": "+1 (928) 437-2593",
+      "address": "422 Poplar Street, Gardners, Utah, 1883",
+      "registered": "Sunday, February 7, 2016 1:59 AM"
+    },
+    {
+      "_id": "5b7481992304cc5b7f8f2287",
+      "isActive": true,
+      "picture": "http://placehold.it/64x64",
+      "age": 33,
+      "name": {
+        "first": "Hubbard",
+        "last": "Byers"
+      },
+      "company": "ZOLAR",
+      "email": "hubbard.byers@zolar.ca",
+      "phone": "+1 (968) 582-3222",
+      "address": "331 Hart Place, Russellville, Illinois, 3545",
+      "registered": "Thursday, October 26, 2017 10:25 PM"
+    },
+    {
+      "_id": "5b74819900441778aa0f473d",
+      "isActive": false,
+      "picture": "http://placehold.it/64x64",
+      "age": 39,
+      "name": {
+        "first": "Ballard",
+        "last": "Jefferson"
+      },
+      "company": "SKYBOLD",
+      "email": "ballard.jefferson@skybold.us",
+      "phone": "+1 (981) 470-2679",
+      "address": "257 Albee Square, Sylvanite, Idaho, 1247",
+      "registered": "Sunday, March 29, 2015 11:18 AM"
+    },
+    {
+      "_id": "5b7481996ed3f5b62d3aa139",
+      "isActive": false,
+      "picture": "http://placehold.it/64x64",
+      "age": 32,
+      "name": {
+        "first": "Richards",
+        "last": "Knight"
+      },
+      "company": "AMTAS",
+      "email": "richards.knight@amtas.net",
+      "phone": "+1 (879) 458-3204",
+      "address": "876 Greene Avenue, Belva, Alaska, 8632",
+      "registered": "Tuesday, March 21, 2017 9:03 PM"
+    },
+    {
+      "_id": "5b748199418cc0e2cc825351",
+      "isActive": false,
+      "picture": "http://placehold.it/64x64",
+      "age": 42,
+      "name": {
+        "first": "Benjamin",
+        "last": "Black"
+      },
+      "company": "GLOBOIL",
+      "email": "benjamin.black@globoil.io",
+      "phone": "+1 (815) 543-3221",
+      "address": "319 Seigel Street, Sabillasville, Michigan, 4937",
+      "registered": "Saturday, July 16, 2016 6:36 AM"
+    },
+    {
+      "_id": "5b748199ec38307b40cafff2",
+      "isActive": true,
+      "picture": "http://placehold.it/64x64",
+      "age": 38,
+      "name": {
+        "first": "Thomas",
+        "last": "Navarro"
+      },
+      "company": "SILODYNE",
+      "email": "thomas.navarro@silodyne.info",
+      "phone": "+1 (929) 557-2968",
+      "address": "818 Frost Street, Seymour, South Dakota, 7740",
+      "registered": "Saturday, April 25, 2015 5:37 AM"
+    },
+    {
+      "_id": "5b74819912e20841d7097225",
+      "isActive": false,
+      "picture": "http://placehold.it/64x64",
+      "age": 30,
+      "name": {
+        "first": "Bell",
+        "last": "Obrien"
+      },
+      "company": "EYEWAX",
+      "email": "bell.obrien@eyewax.biz",
+      "phone": "+1 (887) 471-4000",
+      "address": "313 Oxford Walk, Munjor, Ohio, 5060",
+      "registered": "Monday, October 31, 2016 8:44 PM"
+    },
+    {
+      "_id": "5b7481999de2ea21f0a8aa8c",
+      "isActive": false,
+      "picture": "http://placehold.it/64x64",
+      "age": 36,
+      "name": {
+        "first": "Beasley",
+        "last": "Riggs"
+      },
+      "company": "EZENTIA",
+      "email": "beasley.riggs@ezentia.name",
+      "phone": "+1 (930) 589-3098",
+      "address": "923 Stryker Court, Torboy, Florida, 6462",
+      "registered": "Wednesday, September 10, 2014 6:56 PM"
+    },
+    {
+      "_id": "5b7481996d0e5044c883a9b7",
+      "isActive": true,
+      "picture": "http://placehold.it/64x64",
+      "age": 28,
+      "name": {
+        "first": "Chambers",
+        "last": "Neal"
+      },
+      "company": "ISOLOGICA",
+      "email": "chambers.neal@isologica.me",
+      "phone": "+1 (910) 421-2406",
+      "address": "797 Homecrest Court, Darrtown, Tennessee, 1328",
+      "registered": "Sunday, May 11, 2014 3:41 PM"
+    },
+    {
+      "_id": "5b748199d52f26fcd8f63c0c",
+      "isActive": false,
+      "picture": "http://placehold.it/64x64",
+      "age": 47,
+      "name": {
+        "first": "Caroline",
+        "last": "Madden"
+      },
+      "company": "ACLIMA",
+      "email": "caroline.madden@aclima.biz",
+      "phone": "+1 (930) 503-3042",
+      "address": "294 Olive Street, Tooleville, California, 2732",
+      "registered": "Sunday, April 22, 2018 8:55 PM"
+    },
+    {
+      "_id": "5b748199e31213a2f26135f5",
+      "isActive": true,
+      "picture": "http://placehold.it/64x64",
+      "age": 42,
+      "name": {
+        "first": "Whitaker",
+        "last": "Collier"
+      },
+      "company": "QUIZKA",
+      "email": "whitaker.collier@quizka.co.uk",
+      "phone": "+1 (804) 448-3610",
+      "address": "162 Montana Place, Driftwood, Northern Mariana Islands, 9286",
+      "registered": "Tuesday, March 27, 2018 5:58 PM"
+    },
+    {
+      "_id": "5b7481991071908b7e968584",
+      "isActive": true,
+      "picture": "http://placehold.it/64x64",
+      "age": 26,
+      "name": {
+        "first": "Gretchen",
+        "last": "Salinas"
+      },
+      "company": "YOGASM",
+      "email": "gretchen.salinas@yogasm.org",
+      "phone": "+1 (839) 436-2815",
+      "address": "301 Lyme Avenue, Marbury, District Of Columbia, 233",
+      "registered": "Tuesday, March 21, 2017 8:15 PM"
+    },
+    {
+      "_id": "5b748199c651021349bce4be",
+      "isActive": false,
+      "picture": "http://placehold.it/64x64",
+      "age": 40,
+      "name": {
+        "first": "Ashley",
+        "last": "Vance"
+      },
+      "company": "PREMIANT",
+      "email": "ashley.vance@premiant.tv",
+      "phone": "+1 (936) 595-3613",
+      "address": "221 Times Placez, Efland, Federated States Of Micronesia, 7845",
+      "registered": "Friday, November 20, 2015 4:51 PM"
+    },
+    {
+      "_id": "5b748199a22d5b016b1919c0",
+      "isActive": true,
+      "picture": "http://placehold.it/64x64",
+      "age": 44,
+      "name": {
+        "first": "Rodriquez",
+        "last": "Watkins"
+      },
+      "company": "COMTOURS",
+      "email": "rodriquez.watkins@comtours.ca",
+      "phone": "+1 (873) 559-3945",
+      "address": "863 Abbey Court, Harrison, Indiana, 1118",
+      "registered": "Thursday, August 14, 2014 11:06 PM"
+    },
+    {
+      "_id": "5b7481997585a16830dc695f",
+      "isActive": true,
+      "picture": "http://placehold.it/64x64",
+      "age": 28,
+      "name": {
+        "first": "Herrera",
+        "last": "Harrell"
+      },
+      "company": "ZILLA",
+      "email": "herrera.harrell@zilla.us",
+      "phone": "+1 (825) 469-2773",
+      "address": "793 Cypress Avenue, Virgie, North Carolina, 486",
+      "registered": "Wednesday, November 23, 2016 6:34 AM"
+    },
+    {
+      "_id": "5b748199055d455694b1db5d",
+      "isActive": true,
+      "picture": "http://placehold.it/64x64",
+      "age": 38,
+      "name": {
+        "first": "Stacey",
+        "last": "Cline"
+      },
+      "company": "TRI@TRIBALOG",
+      "email": "stacey.cline@tri@tribalog.net",
+      "phone": "+1 (851) 510-2139",
+      "address": "412 Onderdonk Avenue, Elizaville, West Virginia, 5583",
+      "registered": "Friday, November 7, 2014 5:19 PM"
+    }
+  ];
